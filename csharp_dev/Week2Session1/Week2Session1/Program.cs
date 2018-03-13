fecd7e10c68522e45da92e75fe0c9b06f35ff1ed
djulzz <julien.esposito@gmail.com>
2018-03-13 12:18:02 -0700

    First C Sharp  Project
    All member variables of our student class are public, and we are introducing the default constructor, but there is a bug in it.

---
+++ csharp_dev/Week2Session1/Week2Session1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Week2Session1
+{
+    class Program
+    {
+        void Foo( )
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Student s = new Student();
+            //s.student_id = 1;
+            Console.WriteLine(s.student_id);
+            Console.Read();
+        }
+    }
+}
---
+++ csharp_dev/Week2Session1/Week2Session1/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week2Session1
+{
+    public class Student
+    {
+        public int student_id;
+        public int age;
+        public String NameF;
+        public string NameL;
+
+        public Student()
+        {
+            int student_id = 100;
+            int age = 18;
+            String NameF = "Robert";
+            string NameL = "Paulson";
+    }
+    }
+}
