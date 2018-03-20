435144db7155074be437423ecab485e5b1df86b1
djulzz <julien.esposito@gmail.com>
2018-03-20 14:52:44 -0700

    ASP.NET with Visual Studio - SSL Problem
    ASP.NET with Visual Studio - SSL Problem

---
+++ csharp_dev/asp_development/week3/week3/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
+        //conn_string.Server = "localhost";
+        //conn_string.UserID = "root";
+        //conn_string.Password = "";
+        //conn_string.Database = "OurBoard";
+        String cnx = "Database=OurBoard;Data Source=localhost";
+        MySqlConnection con = new MySqlConnection( cnx );
+
+        try
+        {
+            con.Open();
+        } catch( Exception ex )
+        {
+            Response.Write("The connection could not be opened - " + ex.ToString());
+        }
+
+        if( con.State == System.Data.ConnectionState.Open )
+        {
+            Response.Write("Connection established!");
+        } else
+        {
+            Response.Write("Connection Failed!<br/>");
+            Response.Write(con.State.ToString() + "<br/>");
+        }
+    }
+}
---
+++ csharp_dev/week3_bridge/week3_bridge/Elipse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace week3_bridge
+{
+    public class Elipse : IShape
+    {
+        public int First_Axis
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int Second_Axis
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
