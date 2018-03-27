c537d01d812725308fe6857ea16ad7ef4a8fba97
djulzz <julien.esposito@gmail.com>
2018-03-27 11:10:37 -0700

    Introduction to Arrays
    Introduction to Arrays

---
+++ csharp_dev/week4_arrays_collections/week4_arrays_collections/week4_arrays_collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace week4_arrays_collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            for( int i = 0; i < 10; i++ )
+            {
+                myArray[i] = 10 * i;
+                Console.WriteLine("The value at location " + i + " is " + myArray[i]);
+            }
+
+            int value = myArray[10];
+            //for( int i = 0; i < myArray.Length; i++)
+            //{
+            //    Console.WriteLine("The value at location " + i + " is " + myArray[i]);
+            //}
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
