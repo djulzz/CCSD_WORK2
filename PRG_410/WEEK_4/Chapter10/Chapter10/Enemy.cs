143a72d6aab07b8cc41b4faa8aca68845b5bd7a9
Julien Esposito <julien.esposito@gmail.com>
2018-08-15 13:17:47 -0700

    Ch10
    - Made Enemy Abstract, broke the code in main

--- PRG_410/WEEK_4/Chapter10/Chapter10/Enemy.cs
+++ PRG_410/WEEK_4/Chapter10/Chapter10/Enemy.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace Chapter10
 {
-    public class Enemy
+    // Abstract methods can only fit into abstract classes.
+    // Abstract methods do not have a body (no code associated with them)
+    public abstract class Enemy
     {
+        public abstract void Walk();
         public virtual void Talk()
         {
             Console.WriteLine("Grunt");
@@ -17,6 +20,14 @@ namespace Chapter10
 
     public class BigBoss : Enemy
     {
+        /// <summary>
+        ///  This is redefining the method talk we had in the base class,
+        ///  such that the child can behave differently (so BigBoss can emit a big
+        ///  grunt instead of a regular grunt).. The requirements for that to take
+        ///  place are:
+        ///  - Talk needs to be virtual in the parent class\
+        ///  - Talk needs to be overridden in the Derived class.
+        /// </summary>
         public override void Talk()
         {
             Console.WriteLine("Big Grunt");
